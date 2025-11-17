40d232140a4e790b82d4edd5a300da9ea39590c6
Umeshthota <umeshyadavthota31@gmail.com>
2025-11-17 17:56:18 +0530

    Create app.java

---
+++ app.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int number1 = 10;
+        int number2 = 5;
+        int sum = number1 + number2;
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+    }
+}
